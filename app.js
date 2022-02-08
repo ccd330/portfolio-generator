f4a29accd821a77a189110bda78a26d0252bfc94
claudia.davis <claudiacdavis@gmail.com>
2022-02-07 22:21:45 -0500

    refactor code

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const profileDataArgs = process.argv.slice(2);
 
-//Notice the lack of parentheses around the `profileDataArr` parameter?
 const printProfileData = (profileDataArr) => {
-  //This...
+  // This...
   for (let i = 0; i < profileDataArr.length; i += 1) {
     console.log(profileDataArr[i]);
   }
+
   console.log("================");
 
-  //is the same as this....
+  // Is the same as this...
   profileDataArr.forEach((profileItem) => console.log(profileItem));
 };
+
+printProfileData(profileDataArgs);
